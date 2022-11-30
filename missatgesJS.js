d0b4c4b230eb0ed98863824b16f3438bbbeeecf4
joanpardogine <joanpardo@ginebro.cat>
2022-11-30 19:02:59 +0100

    modified:   missatgesJS.html, .css i .js

--- missatgesJS.js
+++ missatgesJS.js
@@ -1,33 +1,34 @@
+'use strict'
+
 // let caixaDeDialeg = {
   function obreCaixaDeDialeg(titol, missatge) {
     document.getElementById("titol-dialeg").innerHTML = titol;
     document.getElementById("text-dialeg").innerHTML = missatge;
-    document.getElementById("embolcall-dialeg").style.display = "block";
-  }
+    document.getElementById("contenidor-dialeg").style.display = "block";
+  } 
 
   function tancaCaixaDeDialeg() {
-    document.getElementById("embolcall-dialeg").style.display = "none";
+    document.getElementById("contenidor-dialeg").style.display = "none";
   }
 
 // let caixaDeDialeg = {
 //     obre : function (titol, missatge) {
 //       document.getElementById("titol-dialeg").innerHTML = titol;
 //       document.getElementById("text-dialeg").innerHTML = missatge;
-//       document.getElementById("embolcall-dialeg").style.display = "block";
+//       document.getElementById("contenidor-dialeg").style.display = "block";
 //     },
 //     tanca : function () {
-//       document.getElementById("embolcall-dialeg").style.display = "none";
+//       document.getElementById("contenidor-dialeg").style.display = "none";
 //     }
 //   };
 
-
   let barraDeMissatge = {
-    afegir : function (message) {
+    afegir : function (missatgeRebut) {
       let barraDiv = document.createElement("div");
       barraDiv.classList.add("missatge-barra");
-      barraDiv.innerHTML = message;
+      barraDiv.innerHTML = missatgeRebut;
       barraDiv.addEventListener("click", barraDeMissatge.tancar);
-      document.getElementById("embolcall-barra").appendChild(barraDiv);
+      document.getElementById("contenidor-barra").appendChild(barraDiv);
     },
     tancar : function () {
       this.remove();
